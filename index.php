c7ba18e31740f1c5572a29700b69d7b72398f83e
Abel <camilosol123@gmail.com>
2022-09-03 02:36:38 -0500

    t

--- index.php
+++ index.php
@@ -12,9 +12,7 @@ header("Location:indexPage.php");
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
     <link rel="shortcut icon" href="./assets/Perfil/logo.png">
-    <link
-        href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
-        rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="./assets/css/estilos.css">
 </head>
 <body style=" background-image: url(./assets/Fondos/Fondo1.jpg)">
